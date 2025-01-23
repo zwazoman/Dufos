5fb0804ff80f47fc09f213b6767d03ab10381747
zwazoman <141416442+zwazoman@users.noreply.github.com>
2025-01-23 13:43:41 +0100

    esgg
    qgs

---
+++ Dufos/Assets/_Scripts/Test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Grid _grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3Int cellPosition = _grid.WorldToCell(new Vector3(0,10,0));
+        transform.position = _grid.CellToWorld(cellPosition);
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Physics.Raycast(ray, out hit);
+            print(hit.collider.gameObject.name);
+        }
+    }
+}
