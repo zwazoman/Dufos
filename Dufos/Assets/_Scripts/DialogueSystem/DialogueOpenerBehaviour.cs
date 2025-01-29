32ab0bb3f699b3cb8b3a8bbb5f1b56322ca20798
elagmae <lea.pouthier@gmail.com>
2025-01-29 14:46:34 +0100

    finalisation du système de dialogue (proto test)

--- Dufos/Assets/_Scripts/DialogueSystem/DialogueOpenerBehaviour.cs
+++ Dufos/Assets/_Scripts/DialogueSystem/DialogueOpenerBehaviour.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class DialogueOpenerBehaviour : MonoBehaviour
 {
+    public event Action<List<DialogueManager.Choice>> OnChoiceDisplay;
+
     [SerializeField]
     private GameObject _dialogueSystemDisplay;
     [SerializeField]
@@ -11,21 +14,29 @@ public class DialogueOpenerBehaviour : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _speakerDialogue;
     [SerializeField]
-    private List<TextMeshProUGUI> _choiceBoxes;
+    private List<TextMeshProUGUI> _choiceTexts;
     [SerializeField]
     private GameObject _nextDialogueButton;
 
-    private DialogueManager _dialogueManager;
+    private static DialogueManager _dialogueManager;
     private int index;
 
     public void DisplayDialogues(DialogueManager story)
     {
-        _dialogueManager = story;
-        _nextDialogueButton.SetActive(true);
-        _dialogueSystemDisplay.SetActive(true);
-        _speakerName.text = story.SpeakerName;
-        _speakerDialogue.text = story.CharacterDialogue[0];
-        index = 0;
+        if (story == null)
+        {
+            _dialogueSystemDisplay.SetActive(false);
+        }
+
+        else
+        {
+            _dialogueManager = story;
+            _nextDialogueButton.SetActive(true);
+            _dialogueSystemDisplay.SetActive(true);
+            _speakerName.text = story.SpeakerName;
+            _speakerDialogue.text = story.CharacterDialogue[0];
+            index = 0;
+        }
     }
 
     public void NextDialogue()
@@ -42,20 +53,30 @@ public class DialogueOpenerBehaviour : MonoBehaviour
             {
                 for(int i = 0; i < _dialogueManager.Choices.Count; i++)
                 {
-                    foreach(var box in _choiceBoxes)
-                    {
-                        box.transform.parent.transform.parent.gameObject.SetActive(true);
-                    }
-
-                    _choiceBoxes[i].text = _dialogueManager.Choices[i].PlayerChoice;
-                    _nextDialogueButton.SetActive(false);
+                    _choiceTexts[i].transform.parent.transform.parent.gameObject.SetActive(true);
+                    _choiceTexts[i].text = _dialogueManager.Choices[i].PlayerChoice;
                 }
+
+                _nextDialogueButton.SetActive(false);
+
+                List<DialogueManager.Choice> paths = new();
+                foreach(var choice in _dialogueManager.Choices)
+                {
+                    paths.Add(choice);
+                }
+
+                OnChoiceDisplay(paths);
             }
 
             else
             {
                 _dialogueSystemDisplay.SetActive(false);
             }
+        }
+
+        else
+        {
+            _dialogueSystemDisplay.SetActive(false);
         }
     }
 }
