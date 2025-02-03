7634073014b5616fee3e21d901d460bd701794ff
zwazoman <141416442+zwazoman@users.noreply.github.com>
2025-02-03 12:52:15 +0100

    iugi

--- Dufos/Assets/_Scripts/Scriptables/SpellData.cs
+++ Dufos/Assets/_Scripts/Scriptables/SpellData.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SpellForm
+{
+    Sphere,
+    Ray,
+    Point
+}
 [CreateAssetMenu(fileName = "Spell", menuName = "Spells")]
 public class SpellData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite UISprite;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int Uses;
+
+    public SpellForm LaunchForm;
+
+    //en fonction de la form, choisir les directions disponibles
+
+    public float MaxRange;
+
+    public bool BypassNear;
+    public float BypassSize { get { return BypassSize; } set { BypassSize = Mathf.Clamp(value,0, MaxRange); } }
+
+    public SpellForm DamageForm;
+
+    public float Damage;
+
+    //mettre un bouton "show" qui fait apparaitre les gizmos ou colorie la grille pour montrer la zone d'effet du spell
 }
