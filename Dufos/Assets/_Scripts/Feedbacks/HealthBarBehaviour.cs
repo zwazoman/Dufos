6ace74e8fa3b0f1049bf875806e0229159733a69
elagmae <lea.pouthier@gmail.com>
2025-02-10 17:54:22 +0100

    fix healthbar

--- Dufos/Assets/_Scripts/Feedbacks/HealthBarBehaviour.cs
+++ Dufos/Assets/_Scripts/Feedbacks/HealthBarBehaviour.cs
@@ -1,14 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBarBehaviour : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _player;
+    private Health _playerHealth;
     [SerializeField]
     private Vector3 _offset;
 
+    private Slider _healthBar;
+
+    private void Awake()
+    {
+        _playerHealth.OnTakeDamage += SliderUpdate;
+    }
+
     private void Update()
     {
-        this.transform.position = _player.transform.position + _offset;
+        this.transform.position = _playerHealth.gameObject.transform.position + _offset;
+    }
+
+    public void SliderUpdate(int health)
+    {
+        _healthBar.value = health;
     }
 }
