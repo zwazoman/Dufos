f62350da3e3d10fd1a4942af9fc9a5260ae98c90
elagmae <lea.pouthier@gmail.com>
2025-02-17 19:58:24 +0100

    test proto visuels implémentation

--- Dufos/Assets/_Scripts/Entities/Spells/SpellVisuals/SpellVisuals.cs
+++ Dufos/Assets/_Scripts/Entities/Spells/SpellVisuals/SpellVisuals.cs
@@ -13,9 +13,17 @@ public class SpellVisuals
 
     }
 
-    public async Task VisualTest1(WayPoint target)
+    public async Task RockProjectile(WayPoint target)
     {
         await Task.Yield();
         Debug.Log("<color=red>test visuel 1</color>" + target.name);
+        SpellVfxManager.Instance.PlayVfx("RockProjectile", target.transform);
+    }
+
+    public async Task CatchingFire(List<WayPoint> targets)
+    {
+        await Task.Yield();
+        //Debug.Log("<color=red>test visuel 1</color>" + targets.name);
+        //SpellVfxManager.Instance.PlayVfx("CatchingFire", targets.transform);
     }
 }
---
+++ Dufos/Assets/_Scripts/Feedbacks/Menuing/EndCombatHandler.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class EndCombatHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _victoryPanel;
+    [SerializeField]
+    private GameObject _defeatPanel;
+    private bool _end = false;
+
+    private void Update()
+    {
+        if (CombatManager.Instance.Entities.Count > 0 && !_end)
+        {
+            foreach (var entity in CombatManager.Instance.Entities)
+            {
+                if (entity.name.Contains("Enemy"))
+                {
+                    break;
+                }
+
+                else
+                {
+                    _end = true;
+                }
+            }
+
+            if (_end)
+            {
+                VictoryEnd();
+            }
+
+            if (CombatManager.Instance.PlayerEntities.Count == 0)
+            {
+                _end = true;
+                DefeatEnd();
+            }
+        }
+    }
+
+    public void VictoryEnd()
+    {
+        print(CombatManager.Instance.EnemyEntities.Count);
+        print("<color=green>no more enemies, you win</color>");
+        // Ajouter le combat au compteur de combats.
+    }
+
+    public void DefeatEnd()
+    {
+        print("<color=red>no more players, you lose</color>");
+    }
+}
